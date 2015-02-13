5e3205f4f94d5973b2114de6dde55f591572da93
mikemsrk <min2033@gmail.com>
2015-02-12 20:13:48 -0800

    update

--- underscore.js
+++ underscore.js
@@ -107,8 +107,8 @@ var reduceRight = function(list,iteratee,start){
 	return start;
 }
 
-var list = [[0, 1], [2, 3], [4, 5]];
-console.log(reduceRight(list, function(a, b) { return a.concat(b); }, []));
+//var list = [[0, 1], [2, 3], [4, 5]];
+//console.log(reduceRight(list, function(a, b) { return a.concat(b); }, []));
 
 /*
 find_.find(list, predicate, [context]) Alias: detect 
@@ -120,5 +120,9 @@ var even = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
 
 var find = function(list,predicate){
 
-	
+	for(var i=0;i<list.length;i++){
+		if(predicate(list[i]))return list[i];
+	}
 }
+
+console.log(find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; }));
