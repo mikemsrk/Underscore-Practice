8670551f7173cdc6b58fe2d285019a03c3ee509e
mikemsrk <min2033@gmail.com>
2015-02-15 15:32:01 -0800

    add rest, compact

--- arrays.js
+++ arrays.js
@@ -20,9 +20,9 @@ _.initial([5, 4, 3, 2, 1]);
 => [5, 4, 3, 2]*/
 
 var initial = function(array, n){
-	if(n === undefined){return array[array.length-1];}
+	if(n === undefined){return array.slice(0,-1);}
 	else{
-		return array.slice(-n);
+		return array.slice(0,-n);
 	}
 }
 
@@ -33,3 +33,53 @@ Returns the last element of an array. Passing n will return the last n elements 
 
 _.last([5, 4, 3, 2, 1]);
 => 1*/
+
+var last = function(array,n){
+	if(n === undefined){return array[array.length-1];}
+	else{return array.slice(-n)}
+}
+
+//console.log(last([5,4,3,2,1],1));
+
+// rest_.rest(array, [index]) Alias: tail, drop 
+// Returns the rest of the elements in an array. Pass an index to return the values of the array from that index onward.
+
+// _.rest([5, 4, 3, 2, 1]);
+// => [4, 3, 2, 1]
+
+var rest = function(array,index){
+	if(index === undefined) return array.slice(1);
+	else return array.slice(index);
+}
+
+//console.log(rest([5,4,3,2,1]));
+
+// compact_.compact(array) 
+// Returns a copy of the array with all falsy values removed. In JavaScript, false, null, 0, "", undefined and NaN are all falsy.
+
+// _.compact([0, 1, false, 2, '', 3]);
+// => [1, 2, 3]
+
+var compact = function(array){
+	var result = [];
+	array.forEach(function(val){
+		if(val) result.push(val);
+	});
+	return result;
+}
+
+//console.log(compact([0,1,false,2,'',3]));
+
+// flatten_.flatten(array, [shallow]) 
+// Flattens a nested array (the nesting can be to any depth). If you pass shallow, the array will only be flattened a single level.
+
+// _.flatten([1, [2], [3, [[4]]]]);
+// => [1, 2, 3, 4];
+
+// _.flatten([1, [2], [3, [[4]]]], true);
+// => [1, 2, 3, [[4]]];
+
+var flatten = function(array,shallow){
+
+
+}
